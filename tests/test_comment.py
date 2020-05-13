5c4dd06b63ace029bbfc63a950478c4d14a2fb20
Santa claus <santa@northpole.com>
2020-05-13 21:23:19 +0300

    confirmation of tests

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -4,12 +4,12 @@ import unittest
 
 class CommentModelTest(unittest.TestCase):
   def setUp(self):
-    self.user_victor = User(username = 'victor',password = 'victor', email = 'victor@ms.com')
-    self.new_pitch = Pitch(id=1,title='Test',description='This is a test pitch',category="interview",user = self.user_victor,upvotes=0,downvotes=0)
-    self.new_comment = Comment(id=1, content='Test comment',user=self.user_victor,pitch=self.new_pitch)
+    self.user_victor = User(username = 'kennedy',password = 'kennedy', email = 'kennedymbithi12@gmail.com')
+    self.new_pitch = Pitch(id=1,title='Test',description='This is a test pitch',category="interview",user = self.user_kennedy,upvotes=0,downvotes=0)
+    self.new_comment = Comment(id=1, content='Test comment',user=self.user_kennedy,pitch=self.new_pitch)
 
 
   def test_check_instance_variables(self):
     self.assertEquals(self.new_comment.content,'Test comment')
-    self.assertEquals(self.new_comment.user,self.user_victor)
+    self.assertEquals(self.new_comment.user,self.user_kennedy)
     self.assertEquals(self.new_comment.pitch,self.new_pitch)
